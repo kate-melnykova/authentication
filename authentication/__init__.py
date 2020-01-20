748023d45b0c49d084000fbf671bff83ab598d63
Kateryna Melnykova <forkatemelnikova@gmail.com>
2020-01-19 16:22:27 -0800

    Update html pages
    Login/logout/registration/update_user form are moved to separate file to improve reusability

--- authentication/__init__.py
+++ authentication/__init__.py
@@ -5,18 +5,20 @@ import json
 from logging import getLogger
 
 from flask import Blueprint, render_template, request, url_for,\
-    redirect, flash, make_response
+    redirect, flash, make_response, current_app
 
 from authentication.auth import crypting
 from authentication.auth.models import User, AnonymousUser
 from authentication.views.wtforms import LoginForm, RegistrationForm, UpdateUserForm
 from authentication.models.exceptions import NotFound, ValidationError
 
+#__all__ = ['User', 'AnonymousUser', 'NotFound', 'ValidationError', 'auth', 'login_required']
+
 auth = Blueprint('auth', __name__, template_folder='templates')
 logger = getLogger(__name__)
 
 
-@auth.before_request
+@auth.before_app_request
 def get_current_user():
     encrypted_username = request.cookies.get('username')
     if encrypted_username is None:
@@ -29,7 +31,7 @@ def get_current_user():
         else:
             try:
                 request.user = User.load(username)
-            except NotFound:
+            except:  #NotFound
                 request.user = AnonymousUser()
 
 
@@ -48,180 +50,136 @@ def login_required(func):
     return wrapped
 
 
-@auth.context_processor
+@auth.app_context_processor
 def inject_user():
     return dict(user=request.user)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    form_error = request.cookies.get('form_error')
-    try:
-        form_error = json.load(form_error)
-    except TypeError:
-        form_error = defaultdict(str)
-    except AttributeError:
-        form_error = defaultdict(str)
-    if not request.user.is_authenticated():
-        login_form = LoginForm(request.form)
-        return render_template('login.html', loginform=login_form)
-    return redirect(url_for('logout'))
-
-
-@auth.route('/login/processing', methods=["POST"])
-def login_processing():
     if request.user.is_authenticated():
-        flash('You are already logged in!')
-        return redirect(url_for('hello_world'))
+        flash('You are already logged in')
+        return redirect(url_for('logout'))
 
-    loginform = LoginForm(request.form)
-    if loginform.validate():
-        username = loginform.username.data
-        password = loginform.password.data
+    login_form = LoginForm(request.form)
+    if request.method == 'GET':
+        return render_template('login.html', loginform=login_form)
+
+    elif not login_form.validate():
+        flash('Error in the form')
+        return render_template('login.html', loginform=login_form)
+
+    # if we reached this line, then we received a POST request and login_form is validated
+    username = login_form.username.data
+    password = login_form.password.data
+    try:
+        user = User.load(username)
+    except:  # NotFound
+        logger.info(f'user {username} is not found')
+        flash("Incorrect credentials: please double-check username")
+        return render_template('login.html', loginform=login_form)
+
+    logger.info(f'Login: user password is {user.password}')
+    logger.info(f'Login: password entered is {password}')
+    if user.verify_password(password):
         r = make_response(redirect(url_for('hello_world')))
-        try:
-            user = User.load(username)
-        except NotFound:
-            logger.info(f'user {username} is not found')
-            r = make_response(redirect(url_for('login')))
-            flash("Incorrect credentials: please double-check username")
-            return r
+        encrypted_username = crypting.aes_encrypt(username)
+        if login_form.rememberme.data:
+            r.set_cookie('username', encrypted_username,
+                         expires=datetime.datetime.now() + datetime.timedelta(days=365))
+            r.set_cookie('first_name', user.first_name,
+                         expires=datetime.datetime.now() + datetime.timedelta(days=365))
 
-        logger.info(f'Login: user password is {user.password}')
-        logger.info(f'Login: password entered is {password}')
-        if user.verify_password(password):
-            encrypted_username = crypting.aes_encrypt(username)
-            if loginform.rememberme.data:
-                r.set_cookie('username', encrypted_username,
-                             expires=datetime.datetime.now() + datetime.timedelta(days=365))
-                r.set_cookie('first_name', user.first_name,
-                             expires=datetime.datetime.now() + datetime.timedelta(days=365))
-
-            else:
-                r.set_cookie('username', encrypted_username)
-                r.set_cookie('first_name', user.first_name)
-            flash('You are successfully logged in!')
-            return r
         else:
-            flash("Incorrect credentials: please double-check username and password")
-            r.set_cookie('form_error', json.dumps(loginform.errors))
-            return redirect(url_for('login'))
+            r.set_cookie('username', encrypted_username)
+            r.set_cookie('first_name', user.first_name)
+        flash('You are successfully logged in!')
+        return r
+    else:
+        flash("Incorrect credentials: please double-check username and password")
+        return render_template('login.html', loginform=login_form)
 
 
-@auth.route('/logout')
+@auth.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
-    return render_template('logout.html')
+    if request.method == 'GET':
+        return render_template('logout.html')
 
-
-@auth.route('/logout/confirmed', methods=["POST"])
-@login_required
-def logout_process():
-    r = make_response(redirect(url_for('login')))
-    r.delete_cookie('username')
-    r.delete_cookie('first_name')
-    flash('Successfully logged out')
-    return r
+    else:
+        r = make_response(redirect(url_for('login')))
+        r.delete_cookie('username')
+        r.delete_cookie('first_name')
+        flash('Successfully logged out')
+        return r
 
 
 @auth.route('/registration', methods=['GET', 'POST'])
 def registration():
+    if request.user.is_authenticated():
+        r = make_response(redirect(url_for('logout')))
+        flash('You are already logged in! Do you want to logout?')
+        return r
+
     if request.method == 'GET':
-        form_error = request.cookies.get('form_error')
-        try:
-            form_error = json.loads(form_error)
-        except TypeError or AttributeError:
-            form_error = dict()
-
-        if request.user.is_authenticated():
-            r = make_response(redirect(url_for('hello_world')))
-            r.delete_cookie('form_error')
-            flash('You are already logged in!')
-            return r
-
         regform = RegistrationForm()
-        for attr in regform.get_attributes():
-            getattr(regform, attr).saved_error = form_error.get(attr, [])
-        r = make_response(render_template('registration.html', regform=regform))
-        r.delete_cookie('form_error')
-        return r
+        return render_template('registration.html', regform=regform)
 
+    else:
+        regform = RegistrationForm(request.form)
+        if not regform.validate():
+            flash('Error: incorrect entry in the form')
+            return render_template('registration.html', regform=regform)
 
-@auth.route('/registration/processing', methods=["POST"])
-def registration_processing():
-    form = RegistrationForm(request.form)
-    if not form.validate():
-        flash('Error: incorrect entry in the form')
-        r = make_response(redirect(url_for('registration')))
-        r.set_cookie('form_error', json.dumps(form.errors))
-        return r
+        if User.exists(regform.username.data):
+            flash('This username is not available')
+            return render_template('registration.html', regform=regform)
 
-    username = form.username.data
-    if not User.exists(username):
-        password = form.password.data
-        first_name = form.first_name.data
-        dob = form.dob.data
-        email = form.email.data
-        User.create(username=username, password=password,
-                    first_name=first_name, dob=dob,
-                    email=email)
+        User.create(username=regform.username.data,
+                    password=regform.password.data,
+                    first_name=regform.first_name.data,
+                    dob=regform.dob.data,
+                    email=regform.email.data)
         flash('Registration is successful! Please login.')
         return redirect(url_for('login'))
 
-    else:
-        flash('This username is not available')
-        return redirect(url_for('registration'))
 
-
-@auth.route('/update_user')
+@auth.route('/update_user', methods=['GET', 'POST'])
 @login_required
 def update_user():
-    form_error = request.cookies.get('form_error')
-    try:
-        form_error = json.loads(form_error)
-    except TypeError or AttributeError:
-        form_error = dict()
+    if request.method == 'GET':
+        form = UpdateUserForm()
+        for attr in form.data.keys():
+            if attr in request.user.get_attributes():
+                cur_value = getattr(request.user, attr)
+                if cur_value != getattr(User, attr).default:
+                    getattr(form, attr).data = cur_value
 
-    form = UpdateUserForm()
-    for attr in form.get_attributes():
-        if attr in request.user.get_attributes():
-            cur_value = getattr(request.user, attr)
-            if cur_value != getattr(User, attr).default:
-                getattr(form, attr).data = cur_value
-
-    for attr in form.get_attributes():
-        getattr(form, attr).saved_error = form_error.get(attr, [])
-    r = make_response(render_template('update_user.html', form=form))
-    r.delete_cookie('form_error')
-    return r
-
-
-@auth.route('/update_user/processing', methods=['POST'])
-@login_required
-def update_user_processing():
-    form = UpdateUserForm(request.form)
-    if not form.validate():
-        flash('Error: incorrect entry in the form')
-        r = make_response(redirect(url_for('update_user')))
-        r.set_cookie('form_error', json.dumps(form.errors))
-        return r
-
-    print(f'Data is {form.data}')
-    print(f'Entered username is {form.username.data}')
-    print(f'Entered password is {form.cur_password.data}')
-    if request.user.verify_password(form.cur_password.data):
-        data = dict()
-        for attr in form.get_attributes():
-            data[attr] = getattr(form, attr).data
-        request.user.update(**data)
-
-        encrypted_username = crypting.aes_encrypt(form.username.data)
-        r = make_response(redirect(url_for('blogpost_recent')))
-        r.set_cookie('username', encrypted_username)
-        r.set_cookie('first_name', form.first_name.data)
-        flash('You are successfully logged in!')
-        return r
+        return render_template('update_user.html', form=form)
     else:
-        flash("Incorrect credentials: please double-check password")
-        return redirect(url_for('update_user'))
+        form = UpdateUserForm(request.form)
+        if not form.validate():
+            flash('Error: incorrect entry in the form')
+            r = make_response(redirect(url_for('update_user')))
+            r.set_cookie('form_error', json.dumps(form.errors))
+            return r
+
+        print(f'Data is {form.data}')
+        print(f'Entered username is {form.username.data}')
+        print(f'Entered password is {form.cur_password.data}')
+        if request.user.verify_password(form.cur_password.data):
+            data = dict()
+            for attr in form.get_attributes():
+                data[attr] = getattr(form, attr).data
+            request.user.update(**data)
+
+            encrypted_username = crypting.aes_encrypt(form.username.data)
+            r = make_response(redirect(url_for('blogpost_recent')))
+            r.set_cookie('username', encrypted_username)
+            r.set_cookie('first_name', form.first_name.data)
+            flash('You are successfully logged in!')
+            return r
+        else:
+            flash("Incorrect credentials: please double-check password")
+            return redirect(url_for('update_user'))
 
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='authentication',
     version='1.0',
-    packages=['authentication'],
-    install_requires=['Flask == 1.1', 'pycryptodome', 'wtforms', 'passlib'],
+    packages=find_packages(),
+    package_data={'authentication': ['templates/*']},
+    install_requires=['Flask >= 1.0', 'pycryptodome', 'wtforms', 'passlib'],
     url='',
     license='MIT',
     author='katyandrey',
