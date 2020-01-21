51970784fc4382a309aabd573e5bf3317b0082e6
Kateryna Melnykova <forkatemelnikova@gmail.com>
2020-01-20 16:55:01 -0800

    Update Form to not allow special symbols in username
    Redis keys use ':' as a separator, so usernames must NOT include ':'. Also, users are not allowed to use other alphabet letters for usernames. Therefore, users are allowed to use only [a-zA-Z0-9] and space, dot, comma, escalation mark, and question mark

--- authentication/views/wtforms.py
+++ authentication/views/wtforms.py
@@ -3,6 +3,7 @@ from typing import List
 from wtforms import Form
 from wtforms import StringField
 from wtforms import Field, PasswordField, BooleanField, DateField, TextAreaField
+from wtforms.validators import ValidationError
 from wtforms.fields.html5 import EmailField
 from wtforms import validators
 
@@ -10,14 +11,22 @@ from wtforms import validators
 strip_filter = lambda x: x.strip() if x else None
 
 
+def no_special_symbols(form, field):
+    if not all(char.isalnum() or char in [' ', ',', '.', '!', '?'] for char in field.data):
+        raise ValidationError('Field has characters that are not allowed!')
+
+
 class LoginForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=15)])
+    username = StringField('Username', [validators.Length(min=4, max=15), no_special_symbols])
     password = PasswordField('Password', [validators.Length(min=6, max=15)])
     rememberme = BooleanField('Remember me?')
 
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.InputRequired(), validators.Length(min=4, max=15)])
+    username = StringField('Username', [validators.InputRequired(),
+                                        validators.Length(min=4, max=15),
+                                        no_special_symbols,
+                                        ])
     password = PasswordField('Password', [validators.InputRequired(), validators.Length(min=6, max=15)])
     confirm = PasswordField('Repeat Password',
                             [validators.InputRequired(),
