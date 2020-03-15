63426183f21e5448841fb68631dc42f274189c5e
Kateryna Melnykova <forkatemelnikova@gmail.com>
2020-03-14 21:57:26 -0700

    Parse requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from setuptools import find_packages
+from pip._internal.req import parse_requirements
 
 with open("README.md", "r+") as fh:
     long_description = fh.read()
@@ -9,7 +10,7 @@ setup(
     version='1.1.0',
     packages=find_packages(),
     package_data={'authentication': ['templates/*']},
-    install_requires=['Flask >= 1.0', 'pycryptodome', 'wtforms', 'passlib', 'redis'],
+    install_requires=parse_requirements('requirements.txt'),
     url='',
     license='MIT',
     author='Kateryna Melnykova',
