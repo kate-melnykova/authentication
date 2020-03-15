01e1a59d0af78c111041e7cfe072a241c6674895
Kateryna Melnykova <forkatemelnikova@gmail.com>
2020-03-14 22:01:37 -0700

    Parse requirements

--- setup.py
+++ setup.py
@@ -2,16 +2,24 @@ from setuptools import setup
 from setuptools import find_packages
 from pip._internal.req import parse_requirements
 import pkg_resources
+import pathlib
 
 with open("README.md", "r+") as fh:
     long_description = fh.read()
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
     name='authentication',
     version='1.1.0',
     packages=find_packages(),
     package_data={'authentication': ['templates/*']},
-    install_requires=pkg_resources('requirements.txt'),
+    install_requires=install_requires,
     url='',
     license='MIT',
     author='Kateryna Melnykova',
